ff83b5ba8b42c7fc68afefb720bd28dd0ec44de4
Gonzalo98k <gonzalo98k@gmail.com>
2024-06-06 12:36:42 -0300

    correccion de errores y de diseño

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 window.sr = ScrollReveal()
 
 sr.reveal(".banner", {
-    duration: 3000,
+    duration: 1800,
     origin: "top",
     distance: "100px"
 });
@@ -14,19 +14,19 @@ sr.reveal(".primaryText", {
 });
 
 sr.reveal(".imgDesktopContainer", {
-    duration: 3000,
+    duration: 1500,
     origin: "right",
     distance: "100px"
 });
 
 sr.reveal(".workingChild", {
-    duration: 3000,
+    duration: 1500,
     origin: "left",
     distance: "100px"
 });
 
 sr.reveal(".card", {
-    duration: 3000,
+    duration: 1500,
     origin: "right",
     distance: "100px"
 });
@@ -38,31 +38,31 @@ sr.reveal(".footerContainer", {
 });
 
 sr.reveal(".workingImg", {
-    duration: 3000,
+    duration: 1500,
     origin: "left",
     distance: "100px"
 });
 
 sr.reveal(".working3", {
-    duration: 3000,
+    duration: 2000,
     origin: "top",
     distance: "100px"
 });
 
 sr.reveal(".faqItem", {
-    duration: 3000,
+    duration: 2000,
     origin: "left",
     distance: "100px"
 });
 
 sr.reveal(".testimonioContainer", {
-    duration: 3000,
+    duration: 1500,
     origin: "top",
     distance: "100px"
 });
 
 sr.reveal(".faqTitle", {
-    duration: 3000,
+    duration: 2000,
     origin: "top",
     distance: "100px"
 });
